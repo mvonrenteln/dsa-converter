b1dc09b308a249a8619888ee4f0270f4e518ddcb
Marc von Renteln <marc@von-renteln.de>
2018-10-18 12:36:45 +0200

    :sparkles: Separates Ausgabe-Verzeichnis für die AP-Zusammenfassung möglich

--- src/main/kotlin/com/github/mvonrenteln/dsa/converter/DsaConverterMain.kt
+++ src/main/kotlin/com/github/mvonrenteln/dsa/converter/DsaConverterMain.kt
@@ -4,7 +4,12 @@ import java.io.File
 import java.io.InputStream
 import kotlin.system.measureTimeMillis
 
-val parameterDescription = "Parameter: 1. Name der Eingabe-Datei, 2. Ausgabe-Verzeichnis (Optional, Default: 'out')"
+val parameterDescription = """Parameter: 1. Name der Eingabe-Datei,
+    |2. Ausgabe-Verzeichnis der Geschichte (Optional, Default: 'out')
+    |3. Ausgabe-Verzeichnis der AP-Übersicht (Optional, Default: 'out')
+""".trimMargin()
+
+private val DEFAULT_OUT = "out"
 
 fun main(args: Array<String>) {
     val time = measureTimeMillis {
@@ -13,12 +18,13 @@ fun main(args: Array<String>) {
             println("Gebe Beispiel-Datei aus.")
             val inputFileName = "example.yaml"
             val input = ClassLoader.getSystemClassLoader().getResourceAsStream(inputFileName)
-            convert(inputFileName, input, File("out"))
+            convert(inputFileName, input, DEFAULT_OUT, DEFAULT_OUT)
         } else {
             val inputFileName = args[0]
             val input = File(inputFileName).inputStream()
-            val outputDir = if (args.size > 1) args[1] else "out"
-            convert(inputFileName, input, File(outputDir))
+            val storyOutputDir = args.getOrElse(1) { DEFAULT_OUT }
+            val apsOutputDir = args.getOrElse(2) { DEFAULT_OUT }
+            convert(inputFileName, input, storyOutputDir, apsOutputDir)
         }
     }
     println("Gesamt-Konvertierung in $time ms abgeschlossen.")
@@ -27,16 +33,16 @@ fun main(args: Array<String>) {
 
 }
 
-private fun convert(inputFileName: String, inputStream: InputStream, outputDir: File) {
+private fun convert(inputFileName: String, inputStream: InputStream, storyOutputDir: String, apsOutputDir: String) {
     val data = loadDataFile<GruppenDaten>(inputStream, inputFileName)
 
-    val adocStoryFile = File(outputDir, File(inputFileName).nameWithoutExtension + ".adoc")
+    val adocStoryFile = File(storyOutputDir, File(inputFileName).nameWithoutExtension + ".adoc")
     StoryAdocFileWriter(adocStoryFile).writeData(data)
     ASCIIDOCTOR.convertFile(adocStoryFile)
 
-    val apsStoryFile = File(outputDir, File(inputFileName).nameWithoutExtension + "_APs.adoc")
-    ApsAdocFileWriter(apsStoryFile).writeData(data)
-    ASCIIDOCTOR.convertFile(apsStoryFile)
+    val adocApsFile = File(apsOutputDir, File(inputFileName).nameWithoutExtension + "_APs.adoc")
+    ApsAdocFileWriter(adocApsFile).writeData(data)
+    ASCIIDOCTOR.convertFile(adocApsFile)
 }
 
 
